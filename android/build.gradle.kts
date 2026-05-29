1e81faa04a1ffc8ab2e4473153c874ec8d92b36f
Ergouf <fjl1107464507@gmail.com>
2026-05-29 08:28:57 +0800

    fix: upgrade AGP 8.2.2 to 8.5.2 and Gradle 8.5 to 8.7 for Kotlin 2.1.0 R8 compat

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("app.cash.paparazzi") version "1.3.4" apply false
