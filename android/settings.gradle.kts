9685322d320c0185f8ecb61df50be1025c802bff
Ergouf <fjl1107464507@gmail.com>
2026-05-29 08:35:57 +0800

    fix: remove Aliyun from pluginManagement to prevent 502 cascade failures

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,8 +1,6 @@
 pluginManagement {
     repositories {
         google()
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/central")
         gradlePluginPortal()
         mavenCentral()
     }
@@ -16,11 +14,11 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        mavenCentral()
+        gradlePluginPortal()
         maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
